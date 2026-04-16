23a4f03674357be0c041ac4d5ba36ada315651f9
maarijmoin <maarijmoin2004@gmail.com>
2026-04-16 06:17:41 +0500

    add div function

--- test/test_operation.py
+++ test/test_operation.py
@@ -1,4 +1,4 @@
-from src.math_operations import add, sub
+from src.math_operations import add, div, sub
 def test_add():
     assert add(2,3)==5
     assert add(-4,5)==1
@@ -10,3 +10,8 @@ def test_sub():
     assert sub(5,1)==4
     assert sub(0,0)==0
     assert sub(5,5)==0
+    
+def test_div():
+    assert div(10,2)==5
+    assert div(9,3)==3
+    assert div(5,2)==2.5
